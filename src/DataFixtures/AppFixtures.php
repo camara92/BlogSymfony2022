6784704530a9ae6792b5b554ae234b29d220932c
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-22 04:51:00 +0200

    return join(' ', ) . '.'; => solution beuge

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+//use App\DataFixtures;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -11,7 +14,18 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+// create a French faker
 
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i <= 15; $i++) {
+            $post = new Post();
+
+            $post->setTitle($faker->sentence($nbWords =2, $varibleNbWords =true));
+            $post->setContent($faker->sentence($nbWords =15, $varibleNbWords =true));
+            $post->setAuthor($faker->name());
+            $post->setCreatedAt($faker->dateTimeBetween('-6 month'));
+            $manager->persist($post);
+        }
         $manager->flush();
     }
 }
